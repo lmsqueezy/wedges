e3dbc1a959112070f94c1eb326fb3df69b434689
Branko Conjic <brankoconjic@gmail.com>
2023-09-08 00:07:27 -0400

    chore (lemon-lab): remove unnecessary classes

--- apps/lemon-lab/src/app/page.tsx
+++ apps/lemon-lab/src/app/page.tsx
@@ -1,13 +1,14 @@
+import PageTitle from "@/components/PageTitle";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="w-full max-w-5xl grow">
-        <h1 className="mb-24 text-center text-4xl tracking-tight">Lemon Lab</h1>
+        <PageTitle>Wedges Playground</PageTitle>
 
         <div className="flex flex-col items-start gap-5">
-          <div className="bg-wg-white-50 flex flex-col gap-5 p-10">
+          <div className="flex flex-col p-10">
             <Link href="avatar" className="underline">
               Avatar
             </Link>
