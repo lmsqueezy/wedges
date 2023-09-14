28bc5c304197d6986533b488defa60e4ff59cebb
Branko Conjic <brankoconjic@gmail.com>
2023-09-14 17:07:13 -0400

    feat (wedges): add avatar group tests

---
+++ packages/wedges/src/components/AvatarGroup/AvatarGroup.test.tsx
@@ -0,0 +1,68 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+import { AvatarGroup } from ".";
+
+const TEST_ID = "wg-avatar-group";
+
+describe("AvatarGroup", () => {
+  it("should forward ref to the HTMLDivElement", () => {
+    const ref = React.createRef<HTMLDivElement>();
+    const { getByTestId } = render(<AvatarGroup ref={ref} data-testid={TEST_ID} items={[]} />);
+
+    expect(getByTestId(TEST_ID)).toBe(ref.current);
+  });
+});
+
+describe("Given an AvatarGroup with custom class", () => {
+  it("should include the custom class name in the class list", () => {
+    const rendered = render(<AvatarGroup className="text-sm" data-testid={TEST_ID} items={[]} />);
+    const root = rendered.getByTestId(TEST_ID);
+
+    expect(root.classList.contains("text-sm")).toBe(true);
+  });
+});
+
+describe("Given an AvatarGroup with 'items' prop", () => {
+  it("should not render any children if 'items' is an empty array", () => {
+    const { getByTestId } = render(<AvatarGroup data-testid={TEST_ID} items={[]} />);
+
+    expect(getByTestId(TEST_ID).children.length).toBe(0);
+  });
+
+  it("should render the correct number of children", () => {
+    const { getByTestId } = render(<AvatarGroup data-testid={TEST_ID} items={[{}, {}, {}]} />);
+
+    expect(getByTestId(TEST_ID).children.length).toBe(3);
+  });
+
+  it("should pass the size to children components when 'size' prop is set", () => {
+    const rendered = render(<AvatarGroup data-testid={TEST_ID} items={[{}]} size="2xl" />);
+    const root = rendered.getByTestId(TEST_ID);
+
+    expect(root.querySelector("span")?.classList.contains("h-16")).toBe(true);
+  });
+
+  it("should apply the correct z-index on children items based on 'previousOnTop' prop", async () => {
+    const { container } = render(
+      <AvatarGroup
+        items={[{ className: "item-1" }, { className: "item-2" }]}
+        previousOnTop={true}
+      />
+    );
+
+    const item1 = container.querySelector(".item-1") as HTMLElement;
+    const item2 = container.querySelector(".item-2") as HTMLElement;
+
+    expect(item1?.style.zIndex).toBe("2");
+    expect(item2?.style.zIndex).toBe("1");
+  });
+});
+
+describe("Given an AvatarGroup with 'moreLabel' prop", () => {
+  it("should render the label", () => {
+    const { getByText } = render(<AvatarGroup items={[]} moreLabel="More" />);
+
+    expect(getByText("More")).not.toBeNull();
+  });
+});
