52a4d08420ac6536179f6054df7df85be96a3e91
Branko Conjic <brankoconjic@gmail.com>
2023-09-14 17:08:12 -0400

    chore (wedges): badge improvements

--- packages/wedges/src/components/Badge/Badge.tsx
+++ packages/wedges/src/components/Badge/Badge.tsx
@@ -4,43 +4,46 @@ import * as React from "react";
 import { cn } from "../../helpers/utils";
 
 /* -------------------------------- Variants -------------------------------- */
-const defaultDarkClasses = "dark:bg-surface dark:border-surface-borders";
+const defaultDarkClasses = "dark:wg-bg-surface dark:border-surface-borders";
 
 const badgeVariants = cva("py-1 px-2 rounded-lg text-sm flex items-center w-fit", {
   variants: {
     color: {
-      default: ["bg-surface text-surface-foreground border-surface-2-borders"],
+      default: ["wg-bg-surface text-surface-foreground border-surface-2-borders"],
       green: [
-        "bg-wg-green-50 border-wg-green-200 text-wg-green-800 dark:text-wg-green",
+        "wg-bg-wg-green-50 border-wg-green-200 text-wg-green-800 dark:text-wg-green",
         defaultDarkClasses,
       ],
       purple: [
-        "bg-wg-purple-50 border-wg-purple-200 text-wg-purple-700 dark:text-wg-purple-400",
+        "wg-bg-wg-purple-50 border-wg-purple-200 text-wg-purple-700 dark:text-wg-purple-400",
         defaultDarkClasses,
       ],
       orange: [
-        "bg-wg-orange-50 border-wg-orange-200 text-wg-orange-800 dark:text-wg-orange",
+        "wg-bg-wg-orange-50 border-wg-orange-200 text-wg-orange-800 dark:text-wg-orange",
         defaultDarkClasses,
       ],
-      red: ["bg-wg-red-50 border-wg-red-200 text-wg-red-700 dark:text-wg-red", defaultDarkClasses],
+      red: [
+        "wg-bg-wg-red-50 border-wg-red-200 text-wg-red-700 dark:text-wg-red",
+        defaultDarkClasses,
+      ],
       pink: [
-        "bg-wg-pink-50 border-wg-pink-200 text-wg-pink-700 dark:text-wg-pink",
+        "wg-bg-wg-pink-50 border-wg-pink-200 text-wg-pink-700 dark:text-wg-pink",
         defaultDarkClasses,
       ],
       blue: [
-        "bg-wg-blue-50 border-wg-blue-200 text-wg-blue-700 dark:text-wg-blue",
+        "wg-bg-wg-blue-50 border-wg-blue-200 text-wg-blue-700 dark:text-wg-blue",
         defaultDarkClasses,
       ],
       yellow: [
-        "bg-wg-yellow-50 border-wg-yellow-300 text-wg-yellow-800 dark:text-wg-yellow",
+        "wg-bg-wg-yellow-50 border-wg-yellow-300 text-wg-yellow-800 dark:text-wg-yellow",
         defaultDarkClasses,
       ],
       primary: [
-        "bg-primary-50 border-primary-200 text-primary-700 dark:text-primary-400",
+        "wg-bg-primary-50 border-primary-200 text-primary-700 dark:text-primary-400",
         defaultDarkClasses,
       ],
       secondary: [
-        "bg-secondary-50 border-secondary-200 text-secondary-700 dark:text-secondary-400",
+        "wg-bg-secondary-50 border-secondary-200 text-secondary-700 dark:text-secondary-400",
         defaultDarkClasses,
       ],
     },
