fe17d7c75c1b16eb6dc5aad8f301d9f1565151bf
Branko Conjic <brankoconjic@gmail.com>
2023-12-14 13:53:01 +0100

    refactor(docs): add support for data-attributes

--- apps/docs/src/components/PropsTable.tsx
+++ apps/docs/src/components/PropsTable.tsx
@@ -10,8 +10,9 @@ export const PropsTable = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement> & {
     content: [ContentItem, ContentItem, ContentItem][];
+    isData?: boolean;
   }
->(({ content, ...otherProps }, ref) => {
+>(({ content, isData = false, ...otherProps }, ref) => {
   // Sort the content array
   const sortedContent = React.useMemo(() => {
     return [...content].sort((a, b) => {
@@ -30,30 +31,32 @@ export const PropsTable = React.forwardRef<
       <table className="m-0 text-sm" {...otherProps}>
         <thead>
           <tr>
-            <th>Prop</th>
+            <th>{isData ? "Data attribute" : "Prop"}</th>
             <th>Type</th>
-            <th>Default</th>
+            {!isData ? <th>Default</th> : null}
           </tr>
         </thead>
 
         <tbody>
           {sortedContent.map((row, index) => (
             <tr key={index}>
-              {row.map((rowItem, rowIndex) => (
-                <td key={`${rowIndex}--${index}`}>
-                  <div className="flex items-center gap-0.5">
-                    {rowItem?.value ? (
-                      <code className="not-prose relative max-w-[280px] break-words rounded bg-surface px-[0.3rem] py-[0.2rem] font-mono text-[13px] text-xs text-surface-900">
-                        {rowItem?.value}
-                      </code>
-                    ) : (
-                      <span className="select-none text-surface-200">/</span>
-                    )}
+              {row.map((rowItem, rowIndex) =>
+                isData && rowIndex === 2 ? null : (
+                  <td key={`${rowIndex}--${index}`}>
+                    <div className="flex items-center gap-0.5">
+                      {rowItem?.value ? (
+                        <code className="not-prose relative break-words rounded bg-surface px-[0.3rem] py-[0.2rem] font-mono text-[13px] text-xs text-surface-900">
+                          {rowItem?.value}
+                        </code>
+                      ) : (
+                        <span className="select-none text-surface-200">/</span>
+                      )}
 
-                    {rowItem.description ? <Tooltip content={rowItem?.description} /> : null}
-                  </div>
-                </td>
-              ))}
+                      {rowItem.description ? <Tooltip content={rowItem?.description} /> : null}
+                    </div>
+                  </td>
+                )
+              )}
             </tr>
           ))}
         </tbody>
