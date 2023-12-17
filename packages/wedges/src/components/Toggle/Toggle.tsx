764403e353c608cfea5d88ab083c89c95aade3dc
Branko Conjic <brankoconjic@gmail.com>
2023-12-18 00:04:45 +0100

    feat(wedges): add Toggle component

---
+++ packages/wedges/src/components/Toggle/Toggle.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import * as TogglePrimitive from "@radix-ui/react-toggle";
+
+import "@radix-ui/react-toggle-group";
+
+import { cn } from "../../helpers/utils";
+import { Button, type ButtonProps } from "../Button";
+
+/* ---------------------------------- Types --------------------------------- */
+export type ToggleElement = React.ElementRef<typeof TogglePrimitive.Root>;
+export type ToggleProps = React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
+  Omit<ButtonProps, "variant"> & {
+    variant?: "outline" | "transparent";
+  };
+
+/* ------------------------------- Components ------------------------------- */
+const Toggle = React.forwardRef<ToggleElement, ToggleProps>(
+  (
+    {
+      after,
+      before,
+      children,
+      className,
+      disabled,
+      isIconOnly = false,
+      shape = "rounded",
+      size,
+      variant = "outline",
+      ...otherProps
+    },
+    ref
+  ) => {
+    return (
+      <TogglePrimitive.Root asChild {...otherProps}>
+        <Button
+          after={after}
+          before={before}
+          className={cn("data-[state=on]:bg-surface-50", className)}
+          disabled={disabled}
+          isIconOnly={isIconOnly}
+          ref={ref}
+          shape={shape}
+          size={size}
+          variant={variant}
+        >
+          {children}
+        </Button>
+      </TogglePrimitive.Root>
+    );
+  }
+);
+
+Toggle.displayName = "Toggle";
+
+export default Toggle;
