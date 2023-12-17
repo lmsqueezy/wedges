806290f41c6c0f9df864def7a0b214c9e712bdcc
Branko Conjic <brankoconjic@gmail.com>
2023-12-18 00:06:49 +0100

    feat(docs): add Toggle Group docs

---
+++ apps/docs/src/examples/toggle-group/preview.tsx
@@ -0,0 +1,13 @@
+import { GridIcon, GridMasonryIcon, TableColumnsIcon, TableRowsIcon } from "@iconicicons/react";
+import { ToggleGroup } from "@lmsqueezy/wedges";
+
+export default function Example() {
+  return (
+    <ToggleGroup.Root orientation="horizontal" defaultValue="grid" type="single">
+      <ToggleGroup.Item value="grid" before={<GridIcon />} />
+      <ToggleGroup.Item value="masonry" before={<GridMasonryIcon />} />
+      <ToggleGroup.Item value="column" before={<TableColumnsIcon />} />
+      <ToggleGroup.Item value="row" before={<TableRowsIcon />} />
+    </ToggleGroup.Root>
+  );
+}
