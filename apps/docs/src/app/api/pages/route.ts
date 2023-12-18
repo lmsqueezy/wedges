165ae99287924f406caa434e94c2e531d35944db
Branko Conjic <brankoconjic@gmail.com>
2023-12-18 18:58:08 +0100

    feat(docs): add 'raw' field to the API

--- apps/docs/src/app/api/pages/route.ts
+++ apps/docs/src/app/api/pages/route.ts
@@ -3,23 +3,13 @@ import { allDocs } from "contentlayer/generated";
 
 export async function GET(_request: NextRequest) {
   const docs = allDocs.map((doc) => {
-    console.log(doc._id);
     return {
       id: doc._id,
       title: doc.title,
       description: doc.description,
+      raw: doc.body.raw,
     };
   });
 
   return NextResponse.json(docs);
 }
-
-/**
- * [
- * {id, title, description},
- * {},
- * {}
- * ]
- *
- *
- */
