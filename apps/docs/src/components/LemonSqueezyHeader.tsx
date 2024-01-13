5559ebb32866308226d85697d199fe61616913de
Branko Conjic <brankoconjic@gmail.com>
2024-01-13 18:41:42 -0500

    refactor(docs): fix prettier errors

--- apps/docs/src/components/LemonSqueezyHeader.tsx
+++ apps/docs/src/components/LemonSqueezyHeader.tsx
@@ -52,7 +52,7 @@ export default function LemonSqueezyHeader({
           href={siteConfig.lemonSqueezyURL}
         >
           <Logomark className="lg:hidden" />
-          <Logo className="hidden w-auto fill-slate-700 lg:block dark:fill-sky-100" />
+          <Logo className="hidden w-auto fill-slate-700 dark:fill-sky-100 lg:block" />
         </a>
 
         <Navigation aria-label="Main Nav" className="hidden self-stretch md:flex">
