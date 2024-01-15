6bd853ce04034de4272a5ff5b6e8f16515f814ea
Branko Conjic <brankoconjic@gmail.com>
2024-01-15 12:45:39 -0500

    refactor: import * as React

--- packages/wedges/src/components/Avatar/Avatar.test.tsx
+++ packages/wedges/src/components/Avatar/Avatar.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import React from "react";
+import * as React from "react";
 import { render, type RenderResult } from "@testing-library/react";
 
 import Avatar from "./Avatar";
