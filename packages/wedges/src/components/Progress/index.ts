d67ba40c43afc4fbe9141989f945a44f084b063a
Branko Conjic <brankoconjic@gmail.com>
2024-01-23 15:09:58 -0500

    feat: add ProgressCircle component

--- packages/wedges/src/components/Progress/index.ts
+++ packages/wedges/src/components/Progress/index.ts
@@ -1,2 +1,5 @@
 export { default as ProgressBar } from "./ProgressBar";
-export type { ProgressElement, ProgressProps } from "./ProgressBar";
+export type { ProgressBarElement, ProgressBarProps } from "./ProgressBar";
+
+export { default as ProgressCircle } from "./ProgressCircle";
+export type { ProgressCircleElement, ProgressCircleProps } from "./ProgressCircle";
