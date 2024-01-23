db8f910be7814dcfdfda1ed1f911733912ba0e36
Vojislav Petkovic <2244412+vpetkovic@users.noreply.github.com>
2024-01-22 22:14:54 -0500

    docs: fix broken github source link (#56)
    * docs: fix broken github link
    
    * refactor: fix prettier formatting
    
    ---------
    
    Co-authored-by: Branko Conjic <brankoconjic@gmail.com>

--- apps/docs/src/examples/index.ts
+++ apps/docs/src/examples/index.ts
@@ -1868,70 +1868,91 @@ export function Example() {
     <div className="inline-flex w-full max-w-[400px] flex-col gap-y-12">
       <ProgressBar
         afterIndicator={<SpinnerIcon className="animate-spin" />}
-        helperText="27MB of 60MB "
+        helperText="27MB of 60MB"
         indicator="50%"
         label="Uploading..."
-        value={50}
         tooltip="A tooltip is a small box that appears when hovering over a UI element, providing additional information."
+        value={50}
       />
 
       <ProgressBar
         afterIndicator={<CheckCircleIcon className="text-wg-green" />}
+        color="green"
         helperText="60MB of 60MB"
         indicator="100%"
         label="Uploading Complete"
-        value={100}
-        color="green"
         tooltip="A tooltip is a small box that appears when hovering over a UI element, providing additional information."
+        value={100}
       />
 
       <ProgressBar
         afterIndicator={<CloseCircleIcon className="text-wg-red" />}
+        color="red"
         helperText={<span className="text-destructive">Oops, something went wrong</span>}
         label="Uploading Failed"
-        value={100}
-        color="red"
         tooltip="A tooltip is a small box that appears when hovering over a UI element, providing additional information."
+        value={100}
       />
 
       <ProgressBar
-        variant="inline"
+        color="green"
         indicator="368"
         label="Class A"
-        value={368}
         max={500}
-        color="green"
+        value={368}
+        variant="inline"
       />
 
       <ProgressBar
-        variant="inline"
+        color="orange"
         indicator="211"
         label="Class B"
-        value={211}
         max={500}
-        color="orange"
+        value={211}
+        variant="inline"
       />
 
       <ProgressBar
-        variant="inline"
+        color="red"
         indicator="96"
         label="Class A"
-        value={96}
         max={500}
-        color="red"
+        value={96}
+        variant="inline"
       />
 
       <ProgressBar
         afterIndicator={<CloseIcon />}
+        color="secondary"
+        description="(23.6MB)"
         indicator={<span className="text-surface-500">50% (9 sec left)</span>}
         label="design system.fig"
-        description="(23.6MB)"
-        value={50}
         tooltip="A tooltip is a small box that appears when hovering over a UI element, providing additional information."
-        color="secondary"
+        value={50}
       />
 
       <ProgressBar indicator="70%" label="Please wait..." value={70} color="secondary" />
+
+      <ProgressBar
+        afterIndicator={<SpinnerIcon className="animate-spin" />}
+        indicator="50%"
+        label="Progress"
+        tooltip="A tooltip is a small box that appears when hovering over a UI element, providing additional information."
+        value={50}
+        variant="inline"
+      />
+
+      <ProgressBar indicator="50% Complete" value={50} variant="inline" color="blue" />
+
+      <ProgressBar
+        afterIndicator={<CloseIcon />}
+        description="35%"
+        value={35}
+        variant="inline"
+        color="yellow"
+      />
+
+      <ProgressBar value={70} color="pink" helperText="(650MB/1.12GB) · 17 seconds remaining" />
     </div>
   );
 }
