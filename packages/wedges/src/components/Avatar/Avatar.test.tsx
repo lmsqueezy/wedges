3b2b341c54f977486e7d2f50c2c263252af9ae74
Branko Conjic <brankoconjic@gmail.com>
2024-05-10 22:08:11 -0400

    test(wedges): improve Avatar tests

--- packages/wedges/src/components/Avatar/Avatar.test.tsx
+++ packages/wedges/src/components/Avatar/Avatar.test.tsx
@@ -8,198 +8,201 @@ const DELAY = 80;
 const FALLBACK_TEXT = "JD";
 const IMAGE_ALT_TEXT = "Alt text example";
 const IMG_SRC = "test.jpg";
-const TEST_ID = "wg-avatar";
 
 describe("Avatar", () => {
-  it("should forward ref", () => {
+  it("should forward ref to HTMLSpanElement", () => {
     const ref = React.createRef<HTMLSpanElement>();
-    const { getByTestId } = render(<Avatar ref={ref} data-testid={TEST_ID} />);
+    render(<Avatar ref={ref} />);
 
-    expect(getByTestId(TEST_ID)).toBe(ref.current);
-  });
-});
-
-describe("Given an Avatar with a working image", () => {
-  const orignalGlobalImage = window.Image;
-  const imageRef = React.createRef<HTMLImageElement>();
-  let rendered: RenderResult;
-
-  // Mock the Image constructor to return a mock image that will call the onload
-  beforeAll(() => {
-    (window.Image as any) = class MockImage {
-      onload: () => void = () => {};
-      src = "";
-      constructor() {
-        setTimeout(() => {
-          this.onload();
-        }, DELAY);
-
-        return this;
-      }
-    };
+    if (ref.current !== null) {
+      expect(ref.current).toBeInstanceOf(HTMLSpanElement);
+    } else {
+      fail("ref.current is null");
+    }
   });
 
-  afterAll(() => {
-    window.Image = orignalGlobalImage;
+  describe("given an image", () => {
+    const orignalGlobalImage = window.Image;
+    const imageRef = React.createRef<HTMLImageElement>();
+    let rendered: RenderResult;
+
+    // Mock the Image constructor to return a mock image that will call the onload
+    beforeAll(() => {
+      (window.Image as any) = class MockImage {
+        onload: () => void = () => {};
+        src = "";
+        constructor() {
+          setTimeout(() => {
+            this.onload();
+          }, DELAY);
+
+          return this;
+        }
+      };
+    });
+
+    afterAll(() => {
+      window.Image = orignalGlobalImage;
+    });
+
+    beforeEach(() => {
+      rendered = render(<Avatar ref={imageRef} src={IMG_SRC} />);
+    });
+
+    it("should render the Lemon Squeezy fallback first", () => {
+      const fallback = rendered.container.querySelector("svg");
+
+      expect(fallback).not.toBeNull();
+    });
+
+    it("should render the image after it has loaded", async () => {
+      const image = await rendered.findByRole("img");
+      const imageSrc = image?.getAttribute("src");
+
+      expect(imageSrc).toBe(IMG_SRC);
+    });
+
+    it("should pass ref to the image", async () => {
+      const image = await rendered.findByRole("img");
+
+      expect(image).toBe(imageRef.current);
+    });
   });
 
-  beforeEach(() => {
-    rendered = render(<Avatar ref={imageRef} src={IMG_SRC} />);
+  describe("given an image, alt and initials", () => {
+    const orignalGlobalImage = window.Image;
+    let rendered: RenderResult;
+
+    // Mock the Image constructor to return a mock image that will call the onload
+    beforeAll(() => {
+      (window.Image as any) = class MockImage {
+        onload: () => void = () => {};
+        src = "";
+        constructor() {
+          setTimeout(() => {
+            this.onload();
+          }, DELAY);
+
+          return this;
+        }
+      };
+    });
+
+    afterAll(() => {
+      window.Image = orignalGlobalImage;
+    });
+
+    beforeEach(() => {
+      rendered = render(<Avatar alt={IMAGE_ALT_TEXT} initials={FALLBACK_TEXT} src={IMG_SRC} />);
+    });
+
+    it("should render the initials fallback first", () => {
+      const fallback = rendered.getByText(FALLBACK_TEXT);
+
+      expect(fallback).not.toBeNull();
+    });
+
+    it("should render the image with alt text after it has loaded", async () => {
+      const image = await rendered.findByRole("img");
+      const imageAlt = image?.getAttribute("alt");
+
+      expect(imageAlt).toBe(IMAGE_ALT_TEXT);
+    });
   });
 
-  it("should render the Lemon Squeezy fallback first", () => {
-    const fallback = rendered.container.querySelector("svg");
+  describe("given an image and children", () => {
+    const orignalGlobalImage = window.Image;
+    let rendered: RenderResult;
 
-    expect(fallback).not.toBeNull();
+    // Mock the Image constructor to return a mock image that will call the onload
+    beforeAll(() => {
+      (window.Image as any) = class MockImage {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onload: () => void = () => {};
+        src = "";
+        constructor() {
+          setTimeout(() => {
+            this.onload();
+          }, DELAY);
+
+          return this;
+        }
+      };
+    });
+
+    afterAll(() => {
+      window.Image = orignalGlobalImage;
+    });
+
+    beforeEach(() => {
+      rendered = render(<Avatar src={IMG_SRC}>{FALLBACK_TEXT}</Avatar>);
+    });
+
+    it("should render the children fallback first", () => {
+      const fallback = rendered.getByText(FALLBACK_TEXT);
+
+      expect(fallback).not.toBeNull();
+    });
+
+    it("should render the image after it has loaded", async () => {
+      const image = await rendered.findByRole("img");
+      const imageSrc = image?.getAttribute("src");
+
+      expect(imageSrc).toBe(IMG_SRC);
+    });
   });
 
-  it("should render the image after it has loaded", async () => {
-    const image = await rendered.findByRole("img");
-    const imageSrc = image?.getAttribute("src");
+  describe("given only children", () => {
+    let rendered: RenderResult;
 
-    expect(imageSrc).toBe(IMG_SRC);
+    beforeEach(() => {
+      rendered = render(
+        <Avatar>
+          <span>{FALLBACK_TEXT}</span>
+        </Avatar>
+      );
+    });
+
+    it("should render the children", () => {
+      const fallback = rendered.getByText(FALLBACK_TEXT);
+
+      expect(fallback).not.toBeNull();
+    });
+
+    it("should allow children to have any width", () => {
+      const root = rendered.container.querySelector(".aspect-auto");
+
+      expect(root).not.toBeNull();
+      expect(root?.classList.contains("aspect-auto")).toBe(true);
+    });
   });
 
-  it("should pass ref to the image", async () => {
-    const image = await rendered.findByRole("img");
+  describe("given an Avatar without image, initials or children", () => {
+    it("should render the default fallback", () => {
+      const { container } = render(<Avatar />);
+      const fallback = container.querySelector("svg");
 
-    expect(image).toBe(imageRef.current);
-  });
-});
-
-describe("Given an Avatar with a working image, alt and initials", () => {
-  const orignalGlobalImage = window.Image;
-  let rendered: RenderResult;
-
-  // Mock the Image constructor to return a mock image that will call the onload
-  beforeAll(() => {
-    (window.Image as any) = class MockImage {
-      onload: () => void = () => {};
-      src = "";
-      constructor() {
-        setTimeout(() => {
-          this.onload();
-        }, DELAY);
-
-        return this;
-      }
-    };
+      expect(fallback).not.toBeNull();
+    });
   });
 
-  afterAll(() => {
-    window.Image = orignalGlobalImage;
+  describe("given an Avatar with status and notification", () => {
+    it("should render the status and notification", () => {
+      const { container } = render(<Avatar notification="green" status="red" />);
+      const status = container.querySelector(".bg-wg-red");
+      const notification = container.querySelector(".bg-wg-green");
+
+      expect(status).not.toBeNull();
+      expect(notification).not.toBeNull();
+    });
   });
 
-  beforeEach(() => {
-    rendered = render(<Avatar alt={IMAGE_ALT_TEXT} initials={FALLBACK_TEXT} src={IMG_SRC} />);
-  });
+  describe("given an Avatar with size", () => {
+    it("should apply correct size classes", () => {
+      const { container } = render(<Avatar size="2xl" />);
+      const size = container.querySelector(".min-w-16");
 
-  it("should render the initials fallback first", () => {
-    const fallback = rendered.getByText(FALLBACK_TEXT);
-
-    expect(fallback).not.toBeNull();
-  });
-
-  it("should render the image with alt text after it has loaded", async () => {
-    const image = await rendered.findByRole("img");
-    const imageAlt = image?.getAttribute("alt");
-
-    expect(imageAlt).toBe(IMAGE_ALT_TEXT);
-  });
-});
-
-describe("Given an Avatar with a working image and children", () => {
-  const orignalGlobalImage = window.Image;
-  let rendered: RenderResult;
-
-  // Mock the Image constructor to return a mock image that will call the onload
-  beforeAll(() => {
-    (window.Image as any) = class MockImage {
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      onload: () => void = () => {};
-      src = "";
-      constructor() {
-        setTimeout(() => {
-          this.onload();
-        }, DELAY);
-
-        return this;
-      }
-    };
-  });
-
-  afterAll(() => {
-    window.Image = orignalGlobalImage;
-  });
-
-  beforeEach(() => {
-    rendered = render(<Avatar src={IMG_SRC}>{FALLBACK_TEXT}</Avatar>);
-  });
-
-  it("should render the children fallback first", () => {
-    const fallback = rendered.getByText(FALLBACK_TEXT);
-
-    expect(fallback).not.toBeNull();
-  });
-
-  it("should render the image after it has loaded", async () => {
-    const image = await rendered.findByRole("img");
-    const imageSrc = image?.getAttribute("src");
-
-    expect(imageSrc).toBe(IMG_SRC);
-  });
-});
-
-describe("Given an Avatar with only children", () => {
-  let rendered: RenderResult;
-
-  beforeEach(() => {
-    rendered = render(
-      <Avatar>
-        <span>{FALLBACK_TEXT}</span>
-      </Avatar>
-    );
-  });
-
-  it("should render the children", () => {
-    const fallback = rendered.getByText(FALLBACK_TEXT);
-
-    expect(fallback).not.toBeNull();
-  });
-
-  it("should allow children to have any width", () => {
-    const root = rendered.container.querySelector(".aspect-auto");
-
-    expect(root).not.toBeNull();
-    expect(root?.classList.contains("aspect-auto")).toBe(true);
-  });
-});
-
-describe("Given an Avatar without image, initilas or children", () => {
-  it("should render the default fallback", () => {
-    const { container } = render(<Avatar />);
-    const fallback = container.querySelector("svg");
-
-    expect(fallback).not.toBeNull();
-  });
-});
-
-describe("Given an Avatar with status and notification", () => {
-  it("should render the status and notification", () => {
-    const { container } = render(<Avatar notification="green" status="red" />);
-    const status = container.querySelector(".bg-wg-red");
-    const notification = container.querySelector(".bg-wg-green");
-
-    expect(status).not.toBeNull();
-    expect(notification).not.toBeNull();
-  });
-});
-
-describe("Given an Avatar with size", () => {
-  it("should apply correct size classes", () => {
-    const { container } = render(<Avatar size="2xl" />);
-    const size = container.querySelector(".min-w-16");
-
-    expect(size).not.toBeNull();
+      expect(size).not.toBeNull();
+    });
   });
 });
--- packages/wedges/src/components/AvatarGroup/AvatarGroup.test.tsx
+++ packages/wedges/src/components/AvatarGroup/AvatarGroup.test.tsx
@@ -12,57 +12,57 @@ describe("AvatarGroup", () => {
 
     expect(getByTestId(TEST_ID)).toBe(ref.current);
   });
-});
 
-describe("given it has a custom class", () => {
-  it("should include the custom class name in the class list", () => {
-    const rendered = render(<AvatarGroup className="text-sm" data-testid={TEST_ID} items={[]} />);
-    const root = rendered.getByTestId(TEST_ID);
+  describe("given it has a custom class", () => {
+    it("should include the custom class name in the class list", () => {
+      const rendered = render(<AvatarGroup className="text-sm" data-testid={TEST_ID} items={[]} />);
+      const root = rendered.getByTestId(TEST_ID);
 
-    expect(root.classList.contains("text-sm")).toBe(true);
-  });
-});
-
-describe("given it has `items` prop", () => {
-  it("should not render any children if 'items' is an empty array", () => {
-    const { getByTestId } = render(<AvatarGroup data-testid={TEST_ID} items={[]} />);
-
-    expect(getByTestId(TEST_ID).children.length).toBe(0);
+      expect(root.classList.contains("text-sm")).toBe(true);
+    });
   });
 
-  it("should render the correct number of children", () => {
-    const { getByTestId } = render(<AvatarGroup data-testid={TEST_ID} items={[{}, {}, {}]} />);
+  describe("given it has `items` prop", () => {
+    it("should not render any children if 'items' is an empty array", () => {
+      const { getByTestId } = render(<AvatarGroup data-testid={TEST_ID} items={[]} />);
 
-    expect(getByTestId(TEST_ID).children.length).toBe(3);
+      expect(getByTestId(TEST_ID).children.length).toBe(0);
+    });
+
+    it("should render the correct number of children", () => {
+      const { getByTestId } = render(<AvatarGroup data-testid={TEST_ID} items={[{}, {}, {}]} />);
+
+      expect(getByTestId(TEST_ID).children.length).toBe(3);
+    });
+
+    it("should pass the size to children components when 'size' prop is set", () => {
+      const rendered = render(<AvatarGroup data-testid={TEST_ID} items={[{}]} size="2xl" />);
+      const root = rendered.getByTestId(TEST_ID);
+
+      expect(root.querySelector("span")?.classList.contains("min-w-16")).toBe(true);
+    });
+
+    it("should apply the correct z-index on children items based on 'previousOnTop' prop", () => {
+      const { container } = render(
+        <AvatarGroup
+          items={[{ className: "item-1" }, { className: "item-2" }]}
+          previousOnTop={true}
+        />
+      );
+
+      const item1 = container.querySelector<HTMLElement>(".item-1");
+      const item2 = container.querySelector<HTMLElement>(".item-2");
+
+      expect(item1?.style.zIndex).toBe("2");
+      expect(item2?.style.zIndex).toBe("1");
+    });
   });
 
-  it("should pass the size to children components when 'size' prop is set", () => {
-    const rendered = render(<AvatarGroup data-testid={TEST_ID} items={[{}]} size="2xl" />);
-    const root = rendered.getByTestId(TEST_ID);
+  describe("given an AvatarGroup with 'moreLabel' prop", () => {
+    it("should render the label", () => {
+      const { getByText } = render(<AvatarGroup items={[]} moreLabel="More" />);
 
-    expect(root.querySelector("span")?.classList.contains("min-w-16")).toBe(true);
-  });
-
-  it("should apply the correct z-index on children items based on 'previousOnTop' prop", () => {
-    const { container } = render(
-      <AvatarGroup
-        items={[{ className: "item-1" }, { className: "item-2" }]}
-        previousOnTop={true}
-      />
-    );
-
-    const item1 = container.querySelector<HTMLElement>(".item-1");
-    const item2 = container.querySelector<HTMLElement>(".item-2");
-
-    expect(item1?.style.zIndex).toBe("2");
-    expect(item2?.style.zIndex).toBe("1");
-  });
-});
-
-describe("given an AvatarGroup with 'moreLabel' prop", () => {
-  it("should render the label", () => {
-    const { getByText } = render(<AvatarGroup items={[]} moreLabel="More" />);
-
-    expect(getByText("More")).not.toBeNull();
+      expect(getByText("More")).not.toBeNull();
+    });
   });
 });
